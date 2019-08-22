f99f4b8e7daa4c3886562f1ee9f9d2cd9b36fa32
Muna Hussien <monaseraj@gmail.com>
2019-08-22 10:16:31 +0300

    first 3 days code js for saudidevorg

---
+++ Day1.js
@@ -0,0 +1,3 @@
+document.write('5+6=' + (5 + 6) + '\n');
+document.write("Let's Start again , coding is fun" + '\n');
+document.write('مرحبا بكم في عالم الجافاسكربت' + '\n');
---
+++ Day2.js
@@ -0,0 +1,12 @@
+/*
+This is my first code
+And I need to witre more comments 😊
+My Name is Muna 
+I just start with the team and following the instracution
+(“النص هنا اليتم طباعته النه داخل التعليقات”)write.document
+*/
+// It is so nice to start coding again after long time stoping.
+
+document.write('5+6=' + (5 + 6) + '\n');
+document.write("Let's Start again , coding is fun" + '\n');
+document.write('مرحبا بكم في عالم الجافاسكربت' + '\n');
