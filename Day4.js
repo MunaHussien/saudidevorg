4fe1810cdf65b369659a46fa1e23d74c51b9a9d5
Muna Hussien <monaseraj@gmail.com>
2019-08-23 20:34:17 +0300

    4 and 5 days code js for saudidevorg

---
+++ Day4.js
@@ -0,0 +1,7 @@
+var number;
+number = 10;
+
+document.write(
+  'Hello, this is my first challenge and I will do it ' + number + ' times\n'
+);
+document.write('I will do it, I will learn JavaScript\n');
