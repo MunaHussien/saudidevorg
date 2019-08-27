c3df5636d7b99f6065afe7c9c370a0dcda8eb042
Muna Hussien <monaseraj@gmail.com>
2019-08-27 13:27:34 +0300

    Days 7 and 8 of saudidevorg

---
+++ Day7.js
@@ -0,0 +1,8 @@
+/* Day 7 Lesson 
+Words Reserved cannot be used in function and varibles:
+these word cannpt be used as identifier, strict mode , parameter name, function or varibles
+*/
+/*the following examples for wrong use it is giving errors and readlin.
+  */
+var const = 0; 
+ function enum(){}
---
+++ Day8.js
@@ -0,0 +1,5 @@
+// Lesson of Day 8.
+// using (;) in one Line.
+var number;
+number = 8;
+document.write(number);
