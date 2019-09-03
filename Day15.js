db3d56abd2fbfeff70ba6b28d3eef75dfa495aae
Muna Hussien <monaseraj@gmail.com>
2019-09-03 19:53:13 +0300

    Day 15

---
+++ Day15.js
@@ -0,0 +1,13 @@
+var number1 = 10;
+var number2 = 15;
+var number3 = 20;
+if (number1 < number2) {
+  if (number2 > number3)
+    document.write(
+      'The first Number =' +
+        number1 +
+        'is greater that the second number = ' +
+        number2
+    );
+  else document.write('The third number is greatest number = ' + number3);
+}
