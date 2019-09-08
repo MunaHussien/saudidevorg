01649f8d7952a29a4463da335100a5432f5c2103
Muna Hussien <monaseraj@gmail.com>
2019-09-08 20:34:11 +0300

    Week 4 Day 20

---
+++ Day20.js
@@ -0,0 +1,11 @@
+var addNumber = 1;
+while (addNumber <= 10) {
+  document.writeln(addNumber);
+  addNumber++;
+}
+
+var SubtractNumber = 10;
+while (SubtractNumber > 0) {
+  document.writeln(SubtractNumber);
+  SubtractNumber--;
+}
