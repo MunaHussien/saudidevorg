28516ffe47d3a7c204a7b942712a5a4a414766f8
Muna Hussien <monaseraj@gmail.com>
2019-09-08 14:36:40 +0300

    Days 17 18 19 of 3rd week

---
+++ Day16.js
@@ -0,0 +1,8 @@
+var number = 20;
+if (number == 10) {
+  document.write('You have 10 new messages.');
+} else if (number == 20) {
+  document.write('You have 20 new messages.');
+} else {
+  document.write('You new messages.');
+}
---
+++ Day17.js
@@ -0,0 +1,15 @@
+var num = 3;
+switch (num) {
+  case 1:
+    document.write('You have a Lecture');
+    break;
+  case 2:
+    document.write('You have 2 lectures this week');
+    break;
+  case 3:
+    document.write('You have 3 lecture this month');
+    break;
+  default:
+    document.write('You have some lectures soon');
+    break;
+}
