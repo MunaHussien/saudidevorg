2e1c6b0e693732de87e5c928f6ca545e5dac95ee
Muna Hussien <monaseraj@gmail.com>
2019-09-18 09:25:23 +0300

    Day30

---
+++ Day30.js
@@ -0,0 +1,8 @@
+var employee = {
+  id: 1111,
+  firstName: 'Muna',
+  lastName: 'Hussien',
+  eyeColor: 'Black'
+};
+document.write('Welcome to SaudiDevOrg ' + employee.firstName);
+document.write(employee['lastName']);
